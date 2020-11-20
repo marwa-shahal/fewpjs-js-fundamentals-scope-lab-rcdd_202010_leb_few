9fb9cc60268b3c1d40271327fda58f9088fc7fd0
binding-event-8377 <marwa.al.shahal@gmail.com>
2020-11-20 18:40:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-animal = ""
+let animal = "";
 
 function myAnimal() {
-  return animal
+  const animal = "dog";
+  return animal;
+  
 }
 
 function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  return animal
+  const animal = "cat";
+  return animal;
 }
 
+const  two = 2;
 function add2(n) {
-  return n + two
+  return n + two;
 
   // Feel free to move things around!
-  two = 2
+ 
 }
